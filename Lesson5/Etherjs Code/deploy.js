0c5726addca959525b4c56648d6320c753c26e74
Sajal Sharma <sajalsharma2408@gmail.com>
2022-12-16 23:06:05 +0530

    compiling our code

--- Lesson5/Etherjs Code/deploy.js
+++ Lesson5/Etherjs Code/deploy.js
@@ -1,5 +1,13 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
 async function main() {
   //compile this code
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
 }
 
 main()
