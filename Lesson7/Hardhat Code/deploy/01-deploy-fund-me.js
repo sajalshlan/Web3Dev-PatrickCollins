791e161961b03cb36fde5bd8976aab380afe6222
Sajal Sharma <sajalsharma2408@gmail.com>
2022-12-19 17:47:56 +0530

    l7 is hell of a ride

--- Lesson7/Hardhat Code/deploy/01-deploy-fund-me.js
+++ Lesson7/Hardhat Code/deploy/01-deploy-fund-me.js
@@ -1,5 +1,38 @@
 //import
 
+const { network } = require("hardhat")
+
 //main function
 
-//calling the main function
+// //calling the main function
+// function deployFunc(hre) {
+//     hre.getNamedAccounts()
+//     hre.deployments()
+// }
+
+// module.exports.default = deployFunc
+
+const { helperConfig, networkConfig } = require("../helper-hardhat-config")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    //if chain is X use address Y
+    //if chain is Z use address A
+    const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+
+    //mock contract for localhost?
+    //if the contract doesn't exist, we deploy a minimal version of it for our local testing - make a deploy script 00
+
+    //we use namedAccounts, a method  given by the ethers library to easily give name to those different private key spots, mentioned in the network section of config file
+
+    //so, when going for localhost or hardhat network, we want to use a mock
+
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [], //put price feed address
+        log: true,
+    })
+}
--- Lesson7/Hardhat Code/hardhat.config.js
+++ Lesson7/Hardhat Code/hardhat.config.js
@@ -1,7 +1,50 @@
 require("@nomicfoundation/hardhat-toolbox")
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
+// require("../tasks/block-number")
+// require("hardhat-gas-reporter")
+// require("solidity-coverage")
 require("hardhat-deploy")
+
+//adding this or operators below so that sometimes if the url was not there, so hardhat won't show so many errors.
+
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL || "https://eth-goerli/example"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "pKey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "aKey"
+const COINMARKET_API_KEY = process.env.COINMARKET_API_KEY || "aKey"
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            //accounts: thanks hardhat!,
+            chainId: 31337,
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            //different network mai different position:
+            31337: 1,
+        },
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        apiKey: COINMARKET_API_KEY,
+    },
     solidity: "0.8.17",
 }
