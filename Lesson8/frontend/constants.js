1680c836b3ff4224f22f50269b30aaccba2ecce7
Sajal Sharma <sajalsharma2408@gmail.com>
2022-12-23 19:29:12 +0530

    updates

---
+++ Lesson8/frontend/constants.js
@@ -0,0 +1,125 @@
+export const contractAddress = "0x71c95911e9a5d330f4d621842ec243ee1343292e";
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "s_priceFeedAddress",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "FundMe__NotOwner",
+    type: "error",
+  },
+  {
+    stateMutability: "payable",
+    type: "fallback",
+  },
+  {
+    inputs: [],
+    name: "MINIMUM_USD",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "cheaperWithdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "fund",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "i_owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "s_addressToAmountFunded",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "s_funders",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "s_priceFeed",
+    outputs: [
+      {
+        internalType: "contract AggregatorV3Interface",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    stateMutability: "payable",
+    type: "receive",
+  },
+];
--- Lesson8/frontend/index.js
+++ Lesson8/frontend/index.js
@@ -1,6 +1,14 @@
-const connect = async () => {
-  const connectBtn = document.getElementById("connectBtn");
+import { ethers } from "./ethers-5.6.min.js";
+import { abi, contractAddress } from "./constants.js";
 
+const connectBtn = document.getElementById("connectBtn");
+const fundBtn = document.getElementById("fundBtn");
+connectBtn.onclick = connect;
+fundBtn.onclick = fund;
+
+console.log(ethers);
+
+async function connect() {
   if (window.ethereum) {
     await ethereum.request({ method: "eth_requestAccounts" });
     console.log("connected to metamask!");
@@ -8,4 +16,25 @@ const connect = async () => {
   } else {
     connectBtn.innerHTML = "Please download Metamask extension";
   }
-};
+}
+
+async function fund() {
+  const ethAmount = "70";
+  if (window.ethereum) {
+    console.log(`Funding with ${ethAmount}`);
+    //provider/ connection to the blockchain
+    //signer/wallet/account / someone with gas
+    //the contract we are connecting to: its ABI and address
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      const transactionResponse = await contract.fund({
+        value: ethers.utils.parseEther(ethAmount),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
