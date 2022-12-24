4a230a4687e34ea6396edab6a95e82b6c2465fea
Sajal Sharma <sajalsharma2408@gmail.com>
2022-12-24 11:53:16 +0530

    done w/ the basic frontend

--- Lesson8/frontend/index.js
+++ Lesson8/frontend/index.js
@@ -3,8 +3,12 @@ import { abi, contractAddress } from "./constants.js";
 
 const connectBtn = document.getElementById("connectBtn");
 const fundBtn = document.getElementById("fundBtn");
+const balanceBtn = document.getElementById("balanceBtn");
+const withdrawBtn = document.getElementById("withdrawBtn");
 connectBtn.onclick = connect;
 fundBtn.onclick = fund;
+balanceBtn.onclick = getBalance;
+withdrawBtn.onclick = withdraw;
 
 console.log(ethers);
 
@@ -19,7 +23,7 @@ async function connect() {
 }
 
 async function fund() {
-  const ethAmount = "70";
+  const ethAmount = document.getElementById("fund").value;
   if (window.ethereum) {
     console.log(`Funding with ${ethAmount}`);
     //provider/ connection to the blockchain
@@ -28,11 +32,62 @@ async function fund() {
 
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
+
+    //the metamask extension will act as the provider here, giving us the RPC url and the account active currently as the signer
+
     const contract = new ethers.Contract(contractAddress, abi, signer);
     try {
       const transactionResponse = await contract.fund({
         value: ethers.utils.parseEther(ethAmount),
       });
+
+      //hey wait for this tx to finish
+      await listenForTransactionMine(transactionResponse, provider);
+      console.log("Done!");
+
+      //listen for the tx to be mined
+      //listen for an event -> later, after learning
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+  console.log(`Mining ${transactionResponse.hash}`);
+
+  //listen for this transaction to finish
+  //but the provider.once() fnc fires a listen function, which js adds to the event loop and will do the console.log("Done!") from above and then come back to this listener, we don't want that, we want this listener to get executed first and then it log Done!, so adding a promise here
+
+  return new Promise((resolve, reject) => {
+    provider.once(transactionResponse.hash, (transactionReceipt) => {
+      console.log(
+        `Completed with ${transactionReceipt.confirmations} confirmations`
+      );
+      resolve();
+    });
+  });
+}
+
+async function getBalance() {
+  if (window.ethereum) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const balance = await provider.getBalance(contractAddress);
+    console.log(ethers.utils.formatEther(balance));
+  }
+}
+
+async function withdraw() {
+  if (window.ethereum) {
+    console.log("withdrawing...");
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+
+    try {
+      const transactionResponse = await contract.withdraw();
+      await listenForTransactionMine(transactionResponse, provider);
+      console.log("done!");
     } catch (error) {
       console.log(error);
     }
