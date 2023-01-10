606d8dfd59573e52b07f7bfe9ba8451332ce25a2
Sajal Sharma <sajalsharma2408@gmail.com>
2023-01-10 20:04:02 +0530

    state variables, constructor, return functions defined

--- Lesson9/Hardhat Lottery Code/contracts/Raffle.sol
+++ Lesson9/Hardhat Lottery Code/contracts/Raffle.sol
@@ -11,14 +11,24 @@ pragma solidity  ^0.8.17;
 
 
 contract Raffle {
+
+    /* State Variables */
     uint private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    error RAFFLE__NotEnoughEthEntered();
 
     constructor(uint entranceFee) {
         i_entranceFee = entranceFee;
     }
 
+    //making enterRaffle payable as people will sending eth, hence we accepting msg.value
+
     function enterRaffle() public payable {
-        
+        if(msg.value<i_entranceFee){
+            revert RAFFLE__NotEnoughEthEntered();
+        }
+        s_players.push(payable(msg.sender));
     }
 
 
@@ -26,6 +36,10 @@ contract Raffle {
 
 function getEntranceFee() public view returns(uint){
     return i_entranceFee;
+}
+
+function getPlayers(uint index) public view returns(address) {
+return s_players[index];
 }
 
  }
