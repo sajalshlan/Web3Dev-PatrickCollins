bcc3c7279f3717cd4ff333e93ebca435ccdab4ec
Sajal Sharma <sajalsharma2408@gmail.com>
2023-01-12 18:51:21 +0530

    done w/ deploy-mocks script

--- Lesson9/Hardhat Lottery Code/deploy/00-deploy-mocks.js
+++ Lesson9/Hardhat Lottery Code/deploy/00-deploy-mocks.js
@@ -1,13 +1,28 @@
-import { network } from "hardhat"
+import { ethers, network } from "hardhat"
 import { developmentChains } from "../helper-hardhat-config"
+
+const BASE_FEE = ethers.utils.parseEther("0.25") //0.25 is the coordinator price fee, it costs 0.25 LINK per request
+const GAS_PRICE_LINK = 1e9 //link per fas - calculated value based on the gas price of the chain
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
+    const args = [BASE_FEE, GAS_PRICE_LINK]
 
     if (developmentChains.includes(network.name)) {
         log("Local network detected, deploying mocks...")
+
         //deploy a mock vrf coordinator
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            args: args,
+            log: true,
+            waitConfirmations: network.config.blockConfirmations || 1,
+        })
+        log("Mocks deployed")
+        log("-------------------------------")
     }
 }
+
+module.exports.tags = ["all", "mocks"]
