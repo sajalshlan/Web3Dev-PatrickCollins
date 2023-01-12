50e6a7a3ca8abe753bdf8783d2536306412ec59f
Sajal Sharma <sajalsharma2408@gmail.com>
2023-01-12 14:31:44 +0530

    done with code cleanup, fixed the bug and compiled

--- Lesson9/Hardhat Lottery Code/contracts/Raffle.sol
+++ Lesson9/Hardhat Lottery Code/contracts/Raffle.sol
@@ -18,7 +18,14 @@ import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
 error RAFFLE__NotEnoughEthEntered();
 error RAFFLE__TransferFailed();
 error RAFFLE_NotOpen();
-error RAFFLE__UpKeepNotNeeded(address currentBalance, uint numberOfPlayers, uint raffleState);
+error RAFFLE__UpKeepNotNeeded(uint currentBalance, uint numberOfPlayers, uint raffleState);
+
+/**
+ * @title a simple raffle contract
+ * @author shlan
+ * @notice this contract is for creating an untamperable decentralized smart contract for lottery
+ * @dev this contracts uses chainlink vrf v2 and automation keepers
+ */
 
 contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
 
@@ -50,7 +57,7 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
     event RequestedRaffleWinner(uint indexed requestId);
     event WinnerPicked(address indexed winner);
     
-
+    /* Functions */
     constructor(address vrfCoordinatorV2, uint entranceFee, bytes32 gasLane, uint64 subscriptionId, uint32 callbackGasLimit, uint interval) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_entranceFee = entranceFee;
@@ -64,6 +71,10 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
 
     //making enterRaffle payable as people will sending eth, hence we accepting msg.value
 
+    /**
+     * @notice fee for entering the raffle, changing the raffle state and pushing the address to player's array
+     * @dev using custom errors and emitting an event 
+     */
     function enterRaffle() public payable {
         if(msg.value<i_entranceFee){
             revert RAFFLE__NotEnoughEthEntered();
@@ -90,7 +101,7 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
      * 4. lottery should be in an 'open' state, meaning when the random number is returning, it should not allow any new players to join in
      */
 
-    function checkUpkeep(bytes calldata /* checkData */) public override returns(bool upkeepNeeded, bytes memory /* performData */) {
+    function checkUpkeep(bytes memory /* checkData */) public override returns(bool upkeepNeeded, bytes memory /* performData */) {
         bool isOpen = (RaffleState.OPEN == s_raffleState);
         bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
         bool hasPlayers = (s_players.length > 0);
@@ -98,14 +109,14 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
         upkeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
     }
 
-     function performUpkeep(bool calldata /* performData */) external override returns() {
+     function performUpkeep(bytes calldata /* performData */) external override {
         //request a random number
         //do something with it
         //make it a 2 transaction process, harder for people to manipulate then
 
         (bool upKeepNeeded, ) = checkUpkeep("");
         if(!upKeepNeeded){
-            revert RAFFLE__UpKeepNotNeeded(address(this).balance, s_players.length, uint(s_raffleState))
+            revert RAFFLE__UpKeepNotNeeded(address(this).balance, s_players.length, uint(s_raffleState));
         }
 
         s_raffleState = RaffleState.CALCULATING;
@@ -137,6 +148,7 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
         emit WinnerPicked(recentWinner);
      }
 
+/* View/Pure functions */
 function getEntranceFee() public view returns(uint){
     return i_entranceFee;
 }
@@ -149,4 +161,19 @@ function getRecentWinner() public view returns(address) {
     return s_recentWinner;
 }
 
+function getRaffleState() public view returns(RaffleState){
+    return s_raffleState;
+}
+
+function getNumWords() public pure returns(uint){
+    return NUM_WORDS;
+}
+
+function getNumberOfPlayers() public view returns(uint){
+    return s_players.length;
+}
+
+function getLatestTimeStamp() public view returns(uint){
+    return s_lastTimeStamp;
+}
 }
