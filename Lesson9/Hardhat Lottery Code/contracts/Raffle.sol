5c11c2cb4ca19c8a1eb5b3619a7cc4f6018af505
Sajal Sharma <sajalsharma2408@gmail.com>
2023-01-12 19:43:35 +0530

    done w/ deploy-raffle script, w/ all constructor args and verify function

--- Lesson9/Hardhat Lottery Code/contracts/Raffle.sol
+++ Lesson9/Hardhat Lottery Code/contracts/Raffle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity  ^0.8.17;
+pragma solidity ^0.8.17;
 
 //Raffle Contract
 
@@ -12,7 +12,6 @@ pragma solidity  ^0.8.17;
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
-
 
 /* Errors */
 error RAFFLE__NotEnoughEthEntered();
@@ -28,7 +27,6 @@ error RAFFLE__UpKeepNotNeeded(uint currentBalance, uint numberOfPlayers, uint ra
  */
 
 contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
-
     /* Type declaration */
     enum RaffleState {
         OPEN,
@@ -51,14 +49,20 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
     uint private s_lastTimeStamp;
     uint private immutable i_interval;
 
-
     /* Events */
     event RaffleEntry(address indexed player);
     event RequestedRaffleWinner(uint indexed requestId);
     event WinnerPicked(address indexed winner);
-    
+
     /* Functions */
-    constructor(address vrfCoordinatorV2, uint entranceFee, bytes32 gasLane, uint64 subscriptionId, uint32 callbackGasLimit, uint interval) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    constructor(
+        address vrfCoordinatorV2,
+        uint entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit,
+        uint interval
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_entranceFee = entranceFee;
         i_gasLane = gasLane;
@@ -73,22 +77,20 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
 
     /**
      * @notice fee for entering the raffle, changing the raffle state and pushing the address to player's array
-     * @dev using custom errors and emitting an event 
+     * @dev using custom errors and emitting an event
      */
     function enterRaffle() public payable {
-        if(msg.value<i_entranceFee){
+        if (msg.value < i_entranceFee) {
             revert RAFFLE__NotEnoughEthEntered();
         }
-        
-        if(s_raffleState != RaffleState.OPEN)
-            revert RAFFLE_NotOpen();
+
+        if (s_raffleState != RaffleState.OPEN) revert RAFFLE_NotOpen();
 
         s_players.push(payable(msg.sender));
 
         //emit an event whenever we update a dynamic array or mapping
         //event named as reverse of function name
-    emit RaffleEntry(msg.sender);
-
+        emit RaffleEntry(msg.sender);
     }
 
     /**
@@ -101,7 +103,9 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
      * 4. lottery should be in an 'open' state, meaning when the random number is returning, it should not allow any new players to join in
      */
 
-    function checkUpkeep(bytes memory /* checkData */) public override returns(bool upkeepNeeded, bytes memory /* performData */) {
+    function checkUpkeep(
+        bytes memory /* checkData */
+    ) public override returns (bool upkeepNeeded, bytes memory /* performData */) {
         bool isOpen = (RaffleState.OPEN == s_raffleState);
         bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
         bool hasPlayers = (s_players.length > 0);
@@ -109,14 +113,18 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
         upkeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
     }
 
-     function performUpkeep(bytes calldata /* performData */) external override {
+    function performUpkeep(bytes calldata /* performData */) external override {
         //request a random number
         //do something with it
         //make it a 2 transaction process, harder for people to manipulate then
 
         (bool upKeepNeeded, ) = checkUpkeep("");
-        if(!upKeepNeeded){
-            revert RAFFLE__UpKeepNotNeeded(address(this).balance, s_players.length, uint(s_raffleState));
+        if (!upKeepNeeded) {
+            revert RAFFLE__UpKeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint(s_raffleState)
+            );
         }
 
         s_raffleState = RaffleState.CALCULATING;
@@ -128,9 +136,9 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
             NUM_WORDS
         );
         emit RequestedRaffleWinner(requestId);
-     }
+    }
 
-     function fulfillRandomWords(uint /* requestId */, uint[] memory randomWords) internal override{
+    function fulfillRandomWords(uint /* requestId */, uint[] memory randomWords) internal override {
         uint indexOfTheWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfTheWinner];
 
@@ -143,41 +151,41 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
         s_lastTimeStamp = block.timestamp;
         //now since we got recent winner, let's transfer them the money
         (bool success, ) = recentWinner.call{value: address(this).balance}("");
-        if(!success) revert RAFFLE__TransferFailed();
+        if (!success) revert RAFFLE__TransferFailed();
 
         emit WinnerPicked(recentWinner);
-     }
+    }
 
-/* View/Pure functions */
-function getEntranceFee() public view returns(uint){
-    return i_entranceFee;
-}
+    /* View/Pure functions */
+    function getEntranceFee() public view returns (uint) {
+        return i_entranceFee;
+    }
 
-function getPlayers(uint index) public view returns(address) {
-return s_players[index];
-}
+    function getPlayers(uint index) public view returns (address) {
+        return s_players[index];
+    }
 
-function getRecentWinner() public view returns(address) {
-    return s_recentWinner;
-}
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
+    }
 
-function getRaffleState() public view returns(RaffleState){
-    return s_raffleState;
-}
+    function getRaffleState() public view returns (RaffleState) {
+        return s_raffleState;
+    }
 
-function getNumWords() public pure returns(uint){
-    return NUM_WORDS;
-}
+    function getNumWords() public pure returns (uint) {
+        return NUM_WORDS;
+    }
 
-function getNumberOfPlayers() public view returns(uint){
-    return s_players.length;
-}
+    function getNumberOfPlayers() public view returns (uint) {
+        return s_players.length;
+    }
 
-function getLatestTimeStamp() public view returns(uint){
-    return s_lastTimeStamp;
-}
+    function getLatestTimeStamp() public view returns (uint) {
+        return s_lastTimeStamp;
+    }
 
-function getNumberOfRequestConfirmations() public pure returns(uint){
-    return REQUEST_CONFIRMATIONS;
-}
+    function getNumberOfRequestConfirmations() public pure returns (uint) {
+        return REQUEST_CONFIRMATIONS;
+    }
 }
--- Lesson9/Hardhat Lottery Code/deploy/01-deploy-raffle.js
+++ Lesson9/Hardhat Lottery Code/deploy/01-deploy-raffle.js
@@ -1,14 +1,59 @@
-const { network } = require("hardhat")
+const { network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+require("dotenv").config
 
 //module.exports = async({getNamedAccounts, deployments}) .....
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = getNamedAccounts()
+    const chainId = network.config.chainId
+    const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
+    let VRFCoordinatorV2address, subscriptionId
 
+    if (developmentChains.includes(network.name)) {
+        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        VRFCoordinatorV2address = VRFCoordinatorV2Mock.address
+
+        //creating a subscription
+        const transactionResponse = await VRFCoordinatorV2Mock.createSubscription()
+        const transactionReceipt = await transactionResponse.wait(1)
+        subscriptionId = transactionReceipt.events[0].args.subId
+
+        //funding a subscription
+        await VRFCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
+    } else {
+        VRFCoordinatorV2address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
+    }
+
+    const entranceFee = networkConfig[chainId]["entranceFee"]
+    const gasLane = networkConfig[chainId]["gasLane"]
+    const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
+    const interval = networkConfig[chainId]["interval"]
+
+    //arguments
+    const args = [
+        VRFCoordinatorV2address,
+        entranceFee,
+        gasLane,
+        subscriptionId,
+        callbackGasLimit,
+        interval,
+    ]
+
+    //deploying
     const raffle = await deploy("Raffle", {
         from: deployer,
-        args: [],
+        args: args,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        await verify(raffle.address, args)
+    }
+    log("------------------------------------------")
 }
+
+module.exports.tags = ["all", "raffle"]
