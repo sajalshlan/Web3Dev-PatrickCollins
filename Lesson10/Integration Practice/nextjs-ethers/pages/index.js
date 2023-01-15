65c44a23819dd3abf9987780509ee578d588907e
Sajal Sharma <sajalsharma2408@gmail.com>
2023-01-15 13:08:56 +0530

    done w/ execute function w/ nextjs and raw ethers

--- Lesson10/Integration Practice/nextjs-ethers/pages/index.js
+++ Lesson10/Integration Practice/nextjs-ethers/pages/index.js
@@ -25,11 +25,119 @@ export default function Home() {
       setIsConnected(false);
     }
   }
+
+  //execute function
+  async function execute() {
+    if (typeof window.ethereum != "undefined") {
+      //contract address - "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+      //abi - got it from simpleStorage.json, in there is an object abi
+      //node connection - got it - metamask
+      //function - yea we got a function to call, store
+
+      const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+      const abi = [
+        {
+          inputs: [
+            {
+              internalType: "string",
+              name: "_name",
+              type: "string",
+            },
+            {
+              internalType: "uint256",
+              name: "_favoriteNumber",
+              type: "uint256",
+            },
+          ],
+          name: "addPerson",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "string",
+              name: "",
+              type: "string",
+            },
+          ],
+          name: "nameToFavoriteNumber",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          name: "people",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "favoriteNumber",
+              type: "uint256",
+            },
+            {
+              internalType: "string",
+              name: "name",
+              type: "string",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "retrieve",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "_favoriteNumber",
+              type: "uint256",
+            },
+          ],
+          name: "store",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+      ];
+
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+      await contract.store(42);
+    }
+  }
+
   return (
     <>
       <h2>hello</h2>
       {isConnected ? (
-        "Connected"
+        <>
+          "Connected"
+          <button onClick={() => execute()}>Execute</button>
+        </>
       ) : (
         <button onClick={() => connect()}>Connect</button>
       )}
