289d5f3b7e039d97ec0595e635a28562b7aead93
Sajal Sharma <sajalsharma2408@gmail.com>
2023-01-15 16:43:51 +0530

    done w/ executing EZ w/ nextjs and moralis

--- Lesson10/Integration Practice/nextjs-moralis/pages/index.js
+++ Lesson10/Integration Practice/nextjs-moralis/pages/index.js
@@ -1,7 +1,17 @@
 import { useMoralis, useWeb3Contract } from "react-moralis";
+import { abi } from "@/constants/abi";
 
 export default function Home() {
   const { enableWeb3, isWeb3Enabled } = useMoralis();
+
+  const { runContractFunction } = useWeb3Contract({
+    abi: abi,
+    contractAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    functionName: "store",
+    params: {
+      _favoriteNumber: 24,
+    },
+  });
 
   return (
     <>
@@ -9,7 +19,7 @@ export default function Home() {
       {isWeb3Enabled ? (
         <>
           "Connected"
-          <button onClick={() => execute()}>Execute</button>
+          <button onClick={() => runContractFunction()}>Execute</button>
         </>
       ) : (
         <button onClick={() => enableWeb3()}>Connect</button>
