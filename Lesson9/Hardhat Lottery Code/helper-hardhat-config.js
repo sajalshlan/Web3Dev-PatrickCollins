e1c7169936ec25fbc688a1f5a20b1ffb5abc990e
Sajal Sharma <sajalsharma2408@gmail.com>
2023-01-16 20:06:08 +0530

    done w/ staging test

--- Lesson9/Hardhat Lottery Code/helper-hardhat-config.js
+++ Lesson9/Hardhat Lottery Code/helper-hardhat-config.js
@@ -1,7 +1,7 @@
 const { ethers } = require("hardhat")
 
 const networkConfig = {
-    4: {
+    5: {
         name: "goerli",
         vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
         entranceFee: ethers.utils.parseEther("0.01"),
