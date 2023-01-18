a2a0b997ad5cef541efaf485b3d57c1d45ff32de
Sajal Sharma <sajalsharma2408@gmail.com>
2023-01-18 13:36:37 +0530

    wrote getEntranceFee with hook and updating the UI but an error

--- Lesson10/nextjs-smart-contract-lottery/components/LotteryEntrance.js
+++ Lesson10/nextjs-smart-contract-lottery/components/LotteryEntrance.js
@@ -1,9 +1,12 @@
 import { abi, contractAddresses } from "@/constants";
-import { useMoralis, useWeb3Contract } from "react-moralis";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { useMoralis, useWeb3Contract, isWeb3Enabled } from "react-moralis";
 
 export const LotteryEntance = () => {
   const { chainId: chainIdHex } = useMoralis();
   const chainId = parseInt(chainIdHex);
+  const [entranceFee, setEntranceFee] = useState("0");
 
   const raffleAddress =
     chainId in contractAddresses ? contractAddresses[chainId][0] : null;
@@ -16,5 +19,30 @@ export const LotteryEntance = () => {
     msgValue: "",
   });
 
-  return <>Hi! lottery ki mkc</>;
+  //getting the entrance fee from the contract using useWeb3Contract hook and useState hook to re-render it
+  const { runContractFunction: getEntranceFee } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: "getEntranceFee",
+    params: {},
+  });
+
+  //useEffect hook to render
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      console.log("useeffect");
+      async function updateUI() {
+        const entranceFeeFromCall = (await getEntranceFee()).toString();
+        setEntranceFee(ethers.utils.formatUnits(entranceFeeFromCall, "ether"));
+        console.log(entranceFee);
+      }
+      updateUI();
+    }
+  }, [isWeb3Enabled]);
+
+  return (
+    <>
+      Hi! lottery ki mkc <div>Entrance Fee: {entranceFee} ETH</div>
+    </>
+  );
 };
